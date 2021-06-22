62d97590bc85a0c2eb05934ef9bb1ef25dca8b93
jamesburgess44 <jamesburgess44@yahoo.com>
2021-06-22 10:53:20 -0600

    I added inputs to collect contestant information

--- contestant.py
+++ contestant.py
@@ -1,2 +1,13 @@
+from userinterface import UserInterface
+
+
 class Contestant:
+    def __init__(self):
+        self.first_name = UserInterface.get_user_input_string("Enter first name")
+        self.last_name = UserInterface.get_user_input_string("Enter last name")
+        self.email = UserInterface.get_user_input_string("Enter email address")
+        self.registration_number = 0
     pass
+
+    def notify(self, is_winner):
+        pass
